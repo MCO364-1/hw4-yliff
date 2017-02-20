4b964e508184d81c728da72e8dc1212933f55425
Yaakov <Yaakov@YaakovLiff>
2017-02-19 22:32:43 -0500

    commit DS2 HW4 WiP

--- src/main/java/mco364/Main.java
+++ src/main/java/mco364/Main.java
@@ -1,20 +1,48 @@
 package mco364;
 
 import java.awt.Robot;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import static mco364.GameOfLife.XX;
+import static mco364.GameOfLife.__;
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-
         clearConsole();
 
+        GameOfLife life = new GameOfLife(18);
+        Scanner kb = new Scanner(System.in);
+
+        System.out.println("Select which oscillation you wish to see:\n"
+                + "1 for Blinker, 2 for Toad, 3 for Beacon, "
+                + "4 for Pulsar, 5 for Pentadecathlon ");
+
+        int selection = kb.nextInt();
+        if (selection == 1) {
+            life.seed(GameOfLife.blinker);
+        }
+        if (selection == 2) {
+            life.seed(GameOfLife.toad);
+        }
+        if (selection == 3) {
+            life.seed(GameOfLife.beacon);
+        }
+        if (selection == 4) {
+            life.seed(GameOfLife.pulsar);
+        }
+        if (selection == 5) {
+            life.seed(GameOfLife.pentadecathlon);
+        } else {
+            System.out.println("Invalid Selection");
+            System.exit(0);
+        }
+
         for (int i = 0; i < 10; i++) {
-            System.out.println(i);
+            System.out.println(life);
+            life.updateToNextGenerationMT(7);
             sleep(1000);
             clearConsole();
         }
